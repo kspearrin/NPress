b3b68dc7e86507f7fed160717dbb79f5ea07deb6
Kyle Spearrin <kyle.spearrin@gmail.com>
2015-09-17 22:59:24 -0400

    various gulp task updates

--- src/Web/gulpfile.js
+++ src/Web/gulpfile.js
@@ -1,12 +1,14 @@
-﻿/// <binding BeforeBuild='less, lib' Clean='clean' ProjectOpened='less, lib' />
+﻿/// <binding BeforeBuild='build' Clean='clean' ProjectOpened='build' />
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
+    rename = require("gulp-rename"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
     project = require("./project.json"),
-    less = require("gulp-less");
+    less = require("gulp-less"),
+    runSequence = require("run-sequence");
 
 var paths = {
     webroot: "./" + project.webroot + "/"
@@ -21,12 +23,19 @@ paths.lessDir = "./Less/";
 paths.cssDir = paths.webroot + "css/";
 paths.jsDir = paths.webroot + "js/";
 
+gulp.task("build", function (cb) {
+    runSequence(
+        "clean",
+        ["lib", "less"],
+        "min");
+});
+
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.cssDir, cb);
+    rimraf(paths.cssDir + "**/*.min.css", cb);
 });
 
 gulp.task("clean:lib", function (cb) {
@@ -35,16 +44,23 @@ gulp.task("clean:lib", function (cb) {
 
 gulp.task("clean", ["clean:js", "clean:css", "clean:lib"]);
 
-gulp.task("min:js", function () {
+gulp.task("min:js", ["clean:js"], function () {
     gulp.src([paths.js, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js"]);
+gulp.task("min:css", ["clean:css"], function () {
+    gulp.src([paths.cssDir + "**/*.css", "!" + paths.cssDir + "**/*.min.css"], { base: "." })
+        .pipe(cssmin())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("."));
+});
 
-gulp.task("lib", function () {
+gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("lib", ["clean:lib"], function () {
     // bootstrap
     gulp.src(paths.bowerDir + "bootstrap/dist/**/*")
         .pipe(gulp.dest(paths.libDir + "bootstrap"));
@@ -68,23 +84,15 @@ gulp.task("lib", function () {
 });
 
 gulp.task("less", function () {
-    gulp.src(paths.lessDir + 'admin.less')
+    gulp.src(paths.lessDir + "admin.less")
         .pipe(less())
         .pipe(gulp.dest(paths.cssDir));
 
-    gulp.src(paths.lessDir + 'admin.less')
-        .pipe(less())
-        .pipe(concat(paths.cssDir + "admin.min.css"))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-
-    gulp.src(paths.lessDir + 'blog.less')
+    gulp.src(paths.lessDir + "blog.less")
         .pipe(less())
         .pipe(gulp.dest(paths.cssDir));
+});
 
-    gulp.src(paths.lessDir + 'blog.less')
-        .pipe(less())
-        .pipe(concat(paths.cssDir + "blog.min.css"))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
+gulp.task("watch", function () {
+    gulp.watch(paths.lessDir + "*.less", ["less"]);
 });
